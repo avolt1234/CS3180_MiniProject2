d9f3c6f966403e9ca08d74b9c0b370ec5c05ae22
Alex Voultos <voultoa@gmail.com>
2019-10-14 09:35:53 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,78 @@
+"""
+Mini Project 2 for Comparative Languages @ Wright State University
+Author: Alexander Voultos
+Date: 10/14/2019
+
+Tokenizes a string input and displays the value of the tokens
+"""
+
+import ply.lex as lex
+
+
+def lexing(inp):
+
+    # Initialize tokens
+    tokens = ["NUM", "SYM", "ADD", "SUBTRACT", "MULTIPLY", "DIVIDE", "LPAREN", "RPAREN", "ERROR"]
+
+    # Ignore space tokens
+    t_ignore = ' \t'
+
+    def t_error(t):
+        print(f'Illegal Character {t}')
+
+    # Token logic and regex for ADD
+    def t_ADD(t):
+        r'\+'
+        print('ADD', end=' ')
+
+    # Token logic and regex for SUBTRACT
+    def t_SUBTRACT(t):
+        r'\-'
+        print('SUBTRACT', end=' ')
+
+    # Token logic and regex for MULTIPLY
+    def t_MULTIPLY(t):
+        r'\*'
+        print("MULTIPLY", end=' ')
+
+    # Token logic and regex for DIVIDE
+    def t_DIVIDE(t):
+        r'/'
+        print("DIVIDE", end=' ')
+
+    # Token logic and regex for LPAREN
+    def t_LPAREN(t):
+        r'\('
+        print("LPAREN", end=' ')
+
+    # Token logic and regex for RPAREN
+    def t_RPAREN(t):
+        r'\)'
+        print("RPAREN", end=' ')
+
+    # Token logic and regex for NUM
+    def t_NUM(t):
+        r'[0-9]+[.]*[0-9]*'
+        print("NUM", end=' ')
+
+    # Token logic and regex for SYM
+    def t_SYM(t):
+        r'[_a-zA-Z][_a-zA-Z]*'
+        print("SYM", end=' ')
+
+    # Build the Lexer
+    lexer = lex.lex()
+
+    # Input data into lexer
+    lexer.input(inp)
+
+    # Tokenize input
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print(tok)
+
+
+lexing("3 99 5 + 4 * 3 + 44")
+
